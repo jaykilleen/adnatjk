8f51cdafdec88febb82bbec9a38a7856f9f9be8d
jaykilleen <me@jaykilleen.com>
2020-02-14 22:36:58 +1000

    Install devise with views, suggested configurations and a root page.

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,13 @@ module Adnatjk
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+    end
   end
 end
