e112957674a07d30bb5400dc5c80b2d81cc8a285
jaykilleen <me@jaykilleen.com>
2020-02-15 13:54:57 +1000

    Green on new session feature

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "user sessions", type: :feature do
 
+  let(:user) { create :user }
+
   it "user must sign in" do
     visit "/users/sign_in"
-    expect(page).to have_current_url("/users/sign_in")
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'Log In'
+    expect(page).to have_content 'Signed in successfully'
   end
 
 end
