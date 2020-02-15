b0ee8fbd497cb05c7188fbe0ba91f30be6ed2818
jaykilleen <me@jaykilleen.com>
2020-02-15 14:27:19 +1000

    Removed incorrect field from factory.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :user do
     name { "Jay Killeen" }
     email { "me@jaykilleen.com" }
-    hourly_rate { 10.00 }
     password { "password" }
     password_confirmation { "password" }
   end
