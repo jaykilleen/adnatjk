232ee60cddbd563dc4299684b05b6b1e06dd30a0
jaykilleen <me@jaykilleen.com>
2020-02-17 16:20:33 +1000

    Fix update name on user registration error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit :sign_up, keys: [:name]
-    devise_parameter_sanitizer.permit :user_update, keys: [:name, :organisation_id]
+    devise_parameter_sanitizer.permit :account_update, keys: [:name, :organisation_id]
   end
 end
