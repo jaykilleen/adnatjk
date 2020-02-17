ac7b62e1384554caf6da63305f163f2a9c18ecc7
jaykilleen <me@jaykilleen.com>
2020-02-17 21:11:02 +1000

    Change seed hourly rate

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ sal = User.create!(name: 'Sal Rogers',     email: 'sal@hotmail.com',   password:
 puts "There are #{User.count} records in the users table."
 
 puts 'Seeding Organisations'
-org1 = Organisation.create!(name: 'Strawberry Farm', hourly_rate: 9.99)
+org1 = Organisation.create!(name: 'Strawberry Farm', hourly_rate: 0.10)
 org2 = Organisation.create!(name: 'Pineapple Farm',  hourly_rate: 20.00)
 org3 = Organisation.create!(name: 'Cafe Tequila',    hourly_rate: 15.00)
 puts "There are #{Organisation.count} records in the organisations table."
@@ -33,6 +33,10 @@ Shift.create!(organisation_id: org2.id, user_id: jay.id, break_length: "30", sta
 Shift.create!(organisation_id: org2.id, user_id: sal.id, break_length: "30", start: Time.parse("2020-01-22 07:30"), finish: Time.parse("2020-01-22 17:00"))
 Shift.create!(organisation_id: org2.id, user_id: sal.id, break_length: "30", start: Time.parse("2020-01-22 07:30"), finish: Time.parse("2020-01-22 17:00"))
 Shift.create!(organisation_id: org2.id, user_id: sal.id, break_length: "60", start: Time.parse("2020-01-22 07:30"), finish: Time.parse("2020-01-22 17:00"))
+Shift.create!(organisation_id: org1.id, user_id: jay.id, break_length: "30", start: Time.parse("2020-01-22 21:00"), finish: Time.parse("2020-01-23 05:00"))
+Shift.create!(organisation_id: org1.id, user_id: jay.id, break_length: "30", start: Time.parse("2020-02-15 21:00"), finish: Time.parse("2020-02-16 05:00"))
+Shift.create!(organisation_id: org1.id, user_id: jay.id, break_length: "30", start: Time.parse("2020-02-16 09:00"), finish: Time.parse("2020-02-16 17:00"))
+
 puts "There are #{Shift.count} records in the organisations table."
 
 puts "Databases #{Rails.env} tables are now successfully seeded!"
